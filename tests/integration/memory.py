8305258c98055644b7b9d55c6f284c81ad3d703a
Rafael Felipe da Silva <995870@clamed.com.br>
2023-11-08 14:17:21 -0300

    test: remocao de teste desnecessario e adicao do teste de integracao de subprocesso com api pyprocess

--- tests/integration/memory.py
+++
@@ -1,5 +0,0 @@
-import random
-
-k = 50000000
-lista = list(range(k))
-print("Object size: ",lista.__sizeof__() / 10**6, "MB")
--- tests/path_test.py
+++ tests/path_test.py
@@ -1,13 +1,18 @@
 import os, sys, unittest
 sys.path.append(os.path.abspath('./src'))
 
+from pyprocess import projectPaths
+
 class pathTest(unittest.TestCase):
 
     def path_test(self):
-        from pyprocess import projectPaths
+        
         current_dir = os.path.dirname(__file__)
         current_dir = os.path.join(current_dir, 'integration')
-        teste = projectPaths(project_path = current_dir, script_name='data/hello.txt')
+        teste = projectPaths(
+            project_path = current_dir, 
+            script_name='data/hello.txt'
+        )
         with open(teste.script_path) as arquivo:
             hello = arquivo.read()
         self.assertEqual(hello,'ola mundo')
