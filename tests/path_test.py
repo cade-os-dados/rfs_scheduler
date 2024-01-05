e078a25d75f54e0e72a5d69b7f543d134e198014
Rafael Felipe da Silva <995870@clamed.com.br>
2024-01-05 11:11:08 -0300

    test: adicionados testes para garantir que os argumentos inseridos estao sendo adicionais corretamente ao scheduler

--- tests/path_test.py
+++ tests/path_test.py
@@ -1,11 +1,16 @@
+# use the command bellow in the root of project
+# python -m unittest discover . -p "*_test.py"
+
 import os, sys, unittest
 sys.path.append(os.path.abspath('./src'))
 
-from pyprocess import projectPaths
+from datetime import datetime
+
+from pyprocess import projectPaths, pyProcess
 
 class pathTest(unittest.TestCase):
 
-    def path_test(self):
+    def test_path(self):
         
         current_dir = os.path.dirname(__file__)
         current_dir = os.path.join(current_dir, 'integration')
@@ -17,5 +22,35 @@ class pathTest(unittest.TestCase):
             hello = arquivo.read()
         self.assertEqual(hello,'ola mundo')
 
+    def test_args_api(self):
+        teste = projectPaths(
+            project_path = 'hello',
+            script_name = 'world.py',
+            args = '--br  --translate'
+        )
+        processo = pyProcess(process_name='ola', scheduled_time=datetime.now(), paths = teste)
+        self.assertEqual(
+            processo.processes_args, 
+            ['hello\\venv\\Scripts\\python.exe', 'hello\\world.py', '--br', '--translate'], 
+            "A api que unifica os argumentos do path do python, script e argumentos adicionais não está funcionando corretamente"
+        )
+
+        # mudando a estrutura de dados para verificar se continuará funcionando
+        teste.args = ['--br', ' --translate']
+        processo = pyProcess(process_name='ola', scheduled_time=datetime.now(), paths = teste)
+        self.assertEqual(
+            processo.processes_args, 
+            ['hello\\venv\\Scripts\\python.exe', 'hello\\world.py', '--br', '--translate'], 
+            "A api que unifica os argumentos do path do python, script e argumentos adicionais não está funcionando corretamente"
+        )
+
+        teste.args = None
+        processo = pyProcess(process_name='ola', scheduled_time=datetime.now(), paths = teste)
+        self.assertEqual(
+            processo.processes_args, 
+            ['hello\\venv\\Scripts\\python.exe', 'hello\\world.py'], 
+            "A api que unifica os argumentos do path do python, script e argumentos adicionais não está funcionando corretamente"
+        )
+    
 if __name__ == "__main__":
     unittest.main()
--- tests/scheduler_test.py
+++ tests/scheduler_test.py
@@ -1,8 +1,12 @@
+# use the command bellow in the root of project
+# python -m unittest discover . -p "*_test.py"
+
 import os, sys, unittest
 sys.path.append(os.path.abspath('./src'))
 
 from datetime import datetime
 from scheduler import Scheduler
+from pyprocess import pyProcess, projectPaths
 from unittest.mock import MagicMock, patch
 
 class TestScheduler(unittest.TestCase):
@@ -22,6 +26,46 @@ class TestScheduler(unittest.TestCase):
             self.scheduler.add_task(self.mock_task, scheduled_time=scheduled_time)
             self.scheduler.run_task(self.mock_task, scheduled_time)
             self.mock_task.assert_called_once()
+
+    def test_add_process(self):
+        processo = pyProcess(
+            paths = projectPaths(
+                project_path="local",
+                script_name="main.py"
+            ),
+            process_name = "hello",
+            scheduled_time = datetime(2022,1,1)
+        )
+        self.scheduler.add_process(pyprocess=processo)
+        self.assertEqual(
+            self.scheduler.processes[0],
+            (
+                ['local\\venv\\Scripts\\python.exe', 'local\\main.py'], 
+                "hello", 
+                datetime(2022,1,1), 
+                None
+            )
+        )
+
+        processo = pyProcess(
+            paths = projectPaths(
+                project_path="local",
+                script_name="main.py",
+                args = '--hello --world'
+            ),
+            process_name = "hello",
+            scheduled_time = datetime(2022,1,1)
+        )
+        self.scheduler.add_process(pyprocess=processo)
+        self.assertEqual(
+            self.scheduler.processes[1],
+            (
+                ['local\\venv\\Scripts\\python.exe', 'local\\main.py', '--hello', '--world'], 
+                "hello", 
+                datetime(2022,1,1), 
+                None
+            )
+        )
 
 if __name__ == '__main__':
     unittest.main()
